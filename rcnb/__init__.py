36d63ce4452f85595dbe08cfb5e4ffdb7193dec8
Chr_ <chr@chrxw.com>
2020-08-26 23:05:53 +0800

    加入用法

--- rcnb/__init__.py
+++ rcnb/__init__.py
@@ -3,8 +3,21 @@
 # @Author       : Chr_
 # @Date         : 2020-08-26 15:28:58
 # @LastEditors  : Chr_
-# @LastEditTime : 2020-08-26 15:54:52
+# @LastEditTime : 2020-08-26 23:05:37
 # @Description  : RCNB
+'''
+
+'''
+USAGE:
+from rcnb import RCNB
+
+encoder = RCNB()
+
+print(encoder.encode("Who NB?"))
+# ȐȼŃƅȓčƞÞƦȻƝƃŖć
+
+print(encoder.decode("ȐĉņþƦȻƝƃŔć"))
+# RCNB!
 '''
 
 from .core import RCNB
