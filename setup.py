af22f1b60db0346a529320c8e5b25800f568a5e8
Chr_ <chr@chrxw.com>
2021-06-15 23:49:53 +0800

    修改版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="rcnb",
-    version="1.0.2",
+    version="1.0.3",
     author="chr_",
     author_email="chr@chrxw.com",
     description="Everything can be encoded into RCNB with Python",
